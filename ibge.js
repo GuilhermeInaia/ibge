6c4ad86db8a88e9cb83b4b64936057ab50afd819
guilhermeinaia <guilherme.inaias@gmail.com>
2022-09-29 09:37:09 -0300

    Atualizando as funções de estado e cidades.

--- ibge.js
+++ ibge.js
@@ -1,33 +1,41 @@
 
+
 fetch('https://servicodados.ibge.gov.br/api/v1/localidades/regioes?orderBy=nome') // incluindo o ?orderBy=nome fica em ordem alfabética.
     .then((resposta) => resposta.json())
     .then((regioes) => {
         regioes.forEach((cadaRegiao) => {
             document.getElementById('regiao').innerHTML += `
-            <option>${cadaRegiao.nome}</option>
+            <option value="${cadaRegiao.id}">${cadaRegiao.nome}</option>
             `;
         });
         
     })
 
+    function procurarEstado(){
+        let id_estado = document.getElementById('regiao').value 
+        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${id_estado}/estados`) 
+        .then((resposta) => resposta.json())
+        .then((estado) => {
+            document.getElementById('estado').innerHTML = " "
+            estado.forEach((cadaEstado) => {
+                document.getElementById('estado').innerHTML += `
+                <option value="${cadaEstado.id}">${cadaEstado.nome}</option>
+                `;
+            });
+        })
+    }
 
-    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome') // incluindo o ?orderBy=nome fica em ordem alfabética.
-    .then((resposta) => resposta.json())
-    .then((estado) => {
-        estado.forEach((cadaEstado) => {
-            document.getElementById('estado').innerHTML += `
-            <option>${cadaEstado.nome}</option>
-            `;
-        });
-    })
-
-    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/microrregioes?orderBy=nome') // incluindo o ?orderBy=nome fica em ordem alfabética.
-    .then((resposta) => resposta.json())
-    .then((cidade) => {
-        cidade.forEach((cadaCidade) => {
-            document.getElementById('cidade').innerHTML += `
-            <option>${cadaCidade.nome}</option>
-            `;
-        });
-    })
+    function procurarCidade() {
+        let id_cidade = document.getElementById('estado').value 
+        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id_cidade}/microrregioes`) // incluindo o ?orderBy=nome fica em ordem alfabética.
+        .then((resposta) => resposta.json())
+        .then((cidade) => {
+            document.getElementById('cidade').innerHTML = " "
+            cidade.forEach((cadaCidade) => {
+                document.getElementById('cidade').innerHTML += `
+                <option>${cadaCidade.nome}</option>
+                `;
+            });
+        })
+    }
 
